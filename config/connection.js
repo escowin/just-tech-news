1039dc4e0d8108c2ea56bc1d46de28fca94aea84
escowin <edwin@escowinart.com>
2022-11-15 11:16:19 -0600

    13.5.6 | setup jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 // import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 // hiding sensitive data from remote repo
-require('dotenv').config()
+require("dotenv").config();
 
 // create connection to the just tech news database. database, user, password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
