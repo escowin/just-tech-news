14472387f42e34ac8ec0cee331679135973b01af
escowin <edwin@escowinart.com>
2022-12-27 13:11:21 -0600

    14.1.6 | controller, mapping & serializing objects; view, iterating & displaying the objects

---
+++ controllers/home-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    // res.render | specifies which template to usee
+    Post.findAll({
+        // 
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // console.log(dbPostData[0]);
+        // dbPostData contains multiples objects (post, _previousDataValues). by first mapping to the post object, the get method then serializes the specified properties of the post object.
+        const posts = dbPostData.map(post => post.get({ plain: true })); 
+        // passing in the serialized array of posts
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes'); // collects & packages everything for server.js
+const routes = require('./controllers/'); // collects & packages everything for server.js
 const sequelize = require('./config/connection'); // connection to Sequelize
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); // frontend connection. this method takes all contents of a folder and serves them as static assets.
 
 // turns on routes
 app.use(routes);
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // turns on connection to database and server
 // - set to true when updating the relationship betwixt tables.
 sequelize.sync({ force: false }).then(() => { // sql DROP TABLE IF EXISTS
